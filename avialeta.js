d872ae55cac9d89d87938991ee1c6a5bffcf2b82
Roman Krivetsky <DishWasherSurfer@gmail.com>
2016-02-04 19:43:53 +0300

    Working...

--- avialeta.js
+++ avialeta.js
@@ -39,24 +39,3 @@ document.addEventListener('DOMContentLoaded', function () {
         spinner.classList.toggle('is-active');
     }
 });
-
-(function() {
-    'use strict';
-    var snackbarContainer = document.querySelector('#demo-snackbar-example');
-    var showSnackbarButton = document.querySelector('#demo-show-snackbar');
-    var handler = function(event) {
-        showSnackbarButton.style.backgroundColor = '';
-    };
-    showSnackbarButton.addEventListener('click', function() {
-        'use strict';
-        showSnackbarButton.style.backgroundColor = '#' +
-        Math.floor(Math.random() * 0xFFFFFF).toString(16);
-        var data = {
-            message: 'Button color changed.',
-            timeout: 0,
-            actionHandler: handler,
-            actionText: 'Undo'
-        };
-        snackbarContainer.MaterialSnackbar.showSnackbar(data);
-    });
-}());
